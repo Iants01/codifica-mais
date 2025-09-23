594469756cb911a181c76ae204592bce43310ce5
SUPORTE <iantds@gmail.com>
2025-09-23 19:45:43 -0300

    exercicio php-a1 e organizacao de pastas

---
+++ PHP-ARRAYS/php-a1.php
@@ -0,0 +1,14 @@
+<?php
+
+$valorConta = readline("Digite o valor da conta: ");
+
+$porcentagemGorjeta = readline("Digite o valor da porcentagem da gorjeta: ");
+
+$valorGorjeta = $valorConta * ($porcentagemGorjeta / 100);
+
+$totalPagar = $valorConta + $valorGorjeta;
+
+echo "\nValor da conta : R$ $valorConta,00" . PHP_EOL;
+echo "Porcentagem da Gorjeta : $porcentagemGorjeta%" . PHP_EOL;
+echo "Valor da Gorjeta : R$ $valorGorjeta,00" . PHP_EOL;
+echo "Total a pagar : R$ $totalPagar,00" . PHP_EOL;
