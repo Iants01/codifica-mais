a9bb52b32d21ebb288e7ba1c33f3bfa262a6d81f
Ian Tavares <iantds@gmail.com>
2025-09-30 18:18:05 -0300

    Comeco exercicio 11

---
+++ PHP-ARRAYS/php-a11.php
@@ -0,0 +1,94 @@
+<?php
+
+function menu() 
+{
+    echo "Sistema de Gerenciamento de Estoque" . PHP_EOL .
+    "a. (1) Adicionar um produto". PHP_EOL .
+    "b. (2) Remover um produto". PHP_EOL .
+    "c. (3) Verificar o estoque". PHP_EOL .
+    "d. (4) Listar o estoque". PHP_EOL .
+    "e. (5) Sair" . PHP_EOL;
+    $opcaoInformada = readline("Escolha uma opção: ");
+    return $opcaoInformada;
+}
+
+
+function adicionarProduto()
+{
+    $produtos = [
+        'codigo' => 0,
+        'nome' => 0,
+        'tamanho' => 0,
+        'cor' => 0,
+        'quantidade' => 0
+    ];
+
+    echo "ADICIONAR PRODUTO AO ESTOQUE" . PHP_EOL;
+
+    $produtos['codigo'] = readline("Digite o codigo do produto: ");
+    $produtos['nome'] = readline("Digite o nome do produto: ");
+    $produtos['tamanho'] = readline("Digite o tamanho do produto: ");
+    $produtos['cor'] = readline("Digite a cor do produto: ");
+    $produtos['quantidade'] = readline("Digite a quantidade: ");
+
+    echo "Novo produto adicionado: " . PHP_EOL;
+
+    foreach($produtos as $key => $produto) {
+        echo "$key : $produto" . PHP_EOL;
+    }
+    
+   return $produtos;
+
+}
+
+function listarEstoque($estoque)
+{
+    foreach($estoque as $produtos) {
+
+        foreach($produtos as $chave => $produto) {
+            echo "ESTOQUE $chave: $produto" . PHP_EOL;
+        }
+    echo PHP_EOL;
+ }
+ 
+}
+
+$estoque[] = 0;
+
+$opcaoInformada = menu();
+
+if($opcaoInformada == 1) {
+
+    while($opcaoInformada == 1) {
+
+        $estoque[] = adicionarProduto();
+
+        $opcaoInformada =  readline("Para adicioanr outro produto digite '1', para retornar ao menu digite qualquer tecla: ");
+
+    }  
+
+};
+
+/*elseif (menu() == 2) {
+    venderProduto();
+} 
+
+elseif (menu() == 3) {
+    verificarEstoque();
+}
+
+elseif ($opcaoInformada == 4) {
+listarEstoque($estoque);
+} 
+
+elseif (menu() == 5) {
+    
+}
+
+else {
+    menu();
+}
+
+*/
+
+listarEstoque($estoque);
