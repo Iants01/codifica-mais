1d7d66fdae446dc0043b4ea2d99000a6243f3d66
Ian Tavares <iantds@gmail.com>
2025-11-13 03:18:11 -0300

    Exercicio 1 Composer Pronto

--- PHP-COMPOSER/CARBON/index.php
+++ PHP-COMPOSER/CARBON/index.php
@@ -4,23 +4,37 @@ require 'vendor/autoload.php';
 
 use Carbon\Carbon;
 
+Carbon::setLocale('pt-BR');
+
+$dataNascimento = readline("Digite sua data de nascimento no formato 'YYYY-MM-DD': ");
+
 $hoje = Carbon::now('America/Sao_Paulo');
 $dia = $hoje->day;
 $nomeDia = $hoje->dayName;
 $mes = $hoje->localeMonth;
 $ano = $hoje->year;
 
-$aniversario = 0 ; 
+$dataNascimentoFormatada = Carbon::parse($dataNascimento, 'America/Sao_Paulo');
 
-echo "$hoje" . PHP_EOL;
-echo "$dia" . PHP_EOL;
-echo "$nomeDia" . PHP_EOL;
-echo "$mes" . PHP_EOL;
-echo "$ano" . PHP_EOL;
+$idade = (int)$dataNascimentoFormatada->diffInYear($hoje);
+
+$diasDeVida = (int)$dataNascimentoFormatada->diffInDay($hoje);
+
+$diaDaSemanaNascimento = $dataNascimentoFormatada->dayName;
 
 
-$segundaData = Carbon::parse('2026-04-27 00:00:00', 'America/Sao_Paulo');
+$dataProximoAniversario = $dataNascimentoFormatada->copy()->addYears($idade+1);
 
-$resultado = $hoje->diffInDays($segundaData);
+$tempoProximoAniversario = (int)$hoje->diffInDay($dataProximoAniversario);
 
-echo "$resultado";
+
+
+echo "Sua data de nascimento é : $dataNascimento" . PHP_EOL;
+
+echo "Sua idade é: $idade anos" . PHP_EOL;
+
+echo "Você possui $diasDeVida dias de vida" . PHP_EOL;
+
+echo "O dia da Semana do seu nascimento foi: $diaDaSemanaNascimento" . PHP_EOL;
+
+echo"Seu próximo aniversário é: $dataProximoAniversario, faltam $tempoProximoAniversario dias" . PHP_EOL;
